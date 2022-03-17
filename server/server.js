1cae428df1bdf2145eb9ce1202194c05be40ce55
Dee <dgrowette@gmail.com>
2022-03-17 12:39:43 -0700

    finished filling in resolvers.js file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
 const {ApolloServer} = requrie('apollo-server-express');
 
 const {typeDefs, resolvers} = require('./schemas');
@@ -9,6 +9,7 @@ const {authMiddleware} = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
